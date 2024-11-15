ec985eb44200b8b291bfab8d816fe5a7fa42fe41
DoctorDragon <teacher_jiang03@qq.com>
2024-11-16 00:07:48 +0800

    convert success

--- Snake.h
+++ Snake.h
@@ -2,11 +2,22 @@
 #define SNAKE_H
 
 #define SNAKESIZE 100//蛇的身体最大节数
-#define MAPWIDTH 100//宽度
-#define MAPHEIGHT 30//高度
+#define MAPWIDTH 35//宽度
+#define MAPHEIGHT 15//高度
 
 #define W 15
 #define H W
 #define S (W * H)
+
+
+
+char material[4] = {'0', 'D', 'Y', ' '};
+enum charPrint
+{
+    WALL = 0,
+    SNAKE,
+    FOOD,
+    AIR
+};
 
 #endif
